1f068375f560fad015fe4a507f0f8ac9bfb55e40
Adrian Shum <me@cshum.com>
2015-09-14 18:13:53 +0800

    bump version

--- test/test.js
+++ test/test.js
@@ -9,11 +9,10 @@ var test = require('tape')
 var levi = require('../')
 var similarity = require('../lib/util/similarity')
 var group = require('../lib/util/group')
-// var jsondown = require('jsondown')
 var H = require('highland')
 
 var lv = levi('./test/db', {
-  // db: jsondown
+  // db: require('jsondown')
 })
 .use(levi.tokenizer())
 .use(levi.stemmer())
